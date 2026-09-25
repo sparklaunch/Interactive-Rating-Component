6fe29ba27f6ae8a7c4966bb29fd8d124ce954c06
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2026-09-26 01:22:44 +0900

    Fix useSearchParams error

--- app/thank-you/page.tsx
+++ app/thank-you/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import thankYou from "../assets/images/thank-you.svg";
 import styles from "./ThankYou.module.css";
 
@@ -9,16 +10,21 @@ export default function ThankYou() {
 	const searchParams = useSearchParams();
 	const rating = searchParams.get("rating");
 	return (
-		<main className={styles.main}>
-			<Image src={thankYou} alt="Thank-you" />
-			<p className={styles.ratingText}>You selected {rating} out of 5</p>
-			<section className={styles.thankYouSection}>
-				<h1 className={styles.thankYouHeader}>Thank you!</h1>
-				<p className={styles.thankYouText}>
-					We appreciate you taking the time to give a rating. if you
-					ever need more support, don&apos;t hesitate to get in touch!
+		<Suspense fallback={null}>
+			<main className={styles.main}>
+				<Image src={thankYou} alt="Thank-you" />
+				<p className={styles.ratingText}>
+					You selected {rating} out of 5
 				</p>
-			</section>
-		</main>
+				<section className={styles.thankYouSection}>
+					<h1 className={styles.thankYouHeader}>Thank you!</h1>
+					<p className={styles.thankYouText}>
+						We appreciate you taking the time to give a rating. if
+						you ever need more support, don&apos;t hesitate to get
+						in touch!
+					</p>
+				</section>
+			</main>
+		</Suspense>
 	);
 }
