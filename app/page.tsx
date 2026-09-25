d105fb2a434b03ff4722b36ea2e2bd38b8c22c08
Jinwook Kim <34114794+sparklaunch@users.noreply.github.com>
2026-09-25 22:02:28 +0900

    Complete Desktop

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+import Image from "next/image";
+import styles from "./Home.module.css";
+import star from "./assets/images/star.svg";
+
 export default function Home() {
-	return <main></main>;
+	return (
+		<main className={styles.main}>
+			<div className={styles.starWrapper}>
+				<Image src={star} alt="" />
+			</div>
+			<h1 className={styles.header}>How did we do?</h1>
+			<p className={styles.text}>
+				Please let us know how we did with your support request. All
+				feedback is appreciated to help us improve our offering!
+			</p>
+			<div className={styles.buttons}>
+				<button className={styles.button}>1</button>
+				<button className={styles.button}>2</button>
+				<button className={styles.button}>3</button>
+				<button className={styles.button}>4</button>
+				<button className={styles.button}>5</button>
+			</div>
+			<button className={styles.submitButton}>SUBMIT</button>
+		</main>
+	);
 }
